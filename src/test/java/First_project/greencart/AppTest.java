6a7c66f05ceab925f62f1f5eb145d747f1b55cd0
Kanchan Mahajan <kanchan.mahajan88@gmail.com>
2021-07-24 13:17:50 +0530

    First Commit

---
+++ src/test/java/First_project/greencart/AppTest.java
@@ -0,0 +1,20 @@
+package First_project.greencart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
